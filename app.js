4db150f46e680a792d95885dc8dfc3d7cb8636e3
valam75 <niranjan.praba1999@gmail.com>
2026-04-04 23:23:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const vehicles = [
+  {
+    owner: 'Niranjan',
+    vehicle: 'Hyundai i20',
+    serviceDate: '2026-05-10',
+    insuranceRenewal: '2026-08-15',
+    pollutionCheck: '2026-06-20'
+  },
+  {
+    owner: 'Rahul',
+    vehicle: 'Honda City',
+    serviceDate: '2026-04-28',
+    insuranceRenewal: '2026-07-30',
+    pollutionCheck: '2026-05-18'
+  }
+];
+
 app.get('/', (req, res) => {
-    res.send('Vehicle Service Reminder App Running Successfully');
+  res.render('index', { vehicles });
 });
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log('Vehicle Service Reminder App running on port 3000');
 });
